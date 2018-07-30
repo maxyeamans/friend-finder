16a06d446b750913e7500b7bf265e1984ea22da0
Max Yeamans <max.yeamans@gmail.com>
2018-07-29 19:31:46 -0700

    Added boilerplate for survey page and express server.

---
+++ server.js
@@ -0,0 +1,12 @@
+// Dependencies
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+// Sets up the Express App
+const app = express();
+const PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
