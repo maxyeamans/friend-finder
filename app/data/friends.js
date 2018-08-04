a526a529e44338077d2207dddac5c9032339f069
Max Yeamans <max.yeamans@gmail.com>
2018-08-04 08:26:41 -0700

    Routing works. Changed questions. Added more default friends.

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,13 +1,83 @@
 friendData = [
     {
-        name: "Someone",
-        photo: "https://maxyeamans.github.io/Basic-Portfolio/assets/images/profile_pic.JPG",
-        scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
+        "name": "Reed Richards",
+        "photo": "http://i.annihil.us/u/prod/marvel/i/mg/d/c0/51127e9337538.jpg",
+        "scores": [
+            "5",
+            "1",
+            "3",
+            "4",
+            "5",
+            "1",
+            "5",
+            "4",
+            "4",
+            "3"
+        ]
     },
     {
-        name: "Someone else",
-        photo: "https://maxyeamans.github.io/Basic-Portfolio/assets/images/profile_pic.JPG",
-        scores: ["4", "4", "4", "1", "4", "4", "2", "2", "4", "1"]
+        "name": "Dr. Stephen Strange",
+        "photo": "http://i.annihil.us/u/prod/marvel/i/mg/5/f0/5261a85a501fe.jpg",
+        "scores": [
+            "4",
+            "1",
+            "2",
+            "2",
+            "4",
+            "5",
+            "3",
+            "5",
+            "4",
+            "2"
+        ]
+    },
+    {
+        "name": "Squirrel Girl",
+        "photo": "http://i.annihil.us/u/prod/marvel/i/mg/8/60/4c0035beb0c55.jpg",
+        "scores": [
+            "1",
+            "3",
+            "5",
+            "4",
+            "5",
+            "4",
+            "2",
+            "1",
+            "2",
+            "4"
+        ]
+    },
+    {
+        "name": "Logan",
+        "photo": "http://i.annihil.us/u/prod/marvel/i/mg/2/60/537bcaef0f6cf.jpg",
+        "scores": [
+            "4",
+            "4",
+            "5",
+            "5",
+            "5",
+            "3",
+            "4",
+            "2",
+            "4",
+            "1"
+        ]
+    },
+    {
+        "name": "I'm Batman",
+        "photo": "https://upload.wikimedia.org/wikipedia/en/1/17/Batman-BenAffleck.jpg",
+        "scores": [
+            "3",
+            "1",
+            "2",
+            "5",
+            "1",
+            "2",
+            "5",
+            "1",
+            "5",
+            "4"
+        ]
     }
 ];
 
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,6 +1,17 @@
 const path = require("path");
 
 module.exports = app => {
+    
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+        console.log("Sending home page")
+    });
+
+    app.get("/home", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+        console.log("Sending home page")
+    });
+
     app.get("/survey", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
         console.log("Sending survey page...");
